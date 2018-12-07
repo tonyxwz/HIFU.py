ba8903bbce41bad5fb32f6bb225b03f026c6aeda
tonyxwz <tonyxwz@yahoo.com>
2018-12-08 00:15:25 +0100

    sphere in surfaces, cuboid in volume

--- pyHIFU/geometric/surfaces.py
+++ pyHIFU/geometric/surfaces.py
@@ -5,19 +5,19 @@ from pyHIFU.geometric.lines import Line, Ray, Segment
 
 class Plane(object):
     """ faster and more simplified """
+
     def __init__(self, p, n):
         self.p = np.array(p)
         self.normal_vector = Vec3.normalize(n)
         self._p_as_assigned = p
         self._n_as_assigned = n
-        abcd = np.ones(4)
-        abcd[0:3] = n
-        abcd[3] = -self.p.dot(n)
-        self.abcd = abcd
+        self.abcd = np.ones(4)
+        self.abcd[0:3] = n
+        self.abcd[3] = -self.p.dot(n)
 
     def __eq__(self, other):
         return (Vec3.are_perpendicular(self.p - other.p, self.normal_vector) and
-            Vec3.are_equal(self.normal_vector, other.normal_vector))
+                Vec3.are_equal(self.normal_vector, other.normal_vector))
 
     def intersect_line(self, line=None, p0=None, vd=None):
         """ intersection point with a line """
@@ -28,25 +28,24 @@ class Plane(object):
             line = Line(np.array(p0), np.array(vd))
         else:
             raise Exception("illegal argument")
-        
+
         b = self.normal_vector.dot(line.d)
         if b == 0:
             return None
         else:
             t = self.normal_vector.dot(self.p - line.p) / b
             return line.to_coordinate(t)
-    
+
     def intersect_plane(self, plane2):
         return "line"
-    
+
     def has_point(self, point=None):
         point = np.array(point)
         return Vec3.are_perpendicular(self.normal_vector, point - self.p)
 
     def has_line(self, line=None):
-        return (self.has_point(point=line.p) and 
-            line.is_perpendicular(vector=self.normal_vector))
-
+        return (self.has_point(point=line.p) and
+                line.is_perpendicular(vector=self.normal_vector))
 
     def get_matrix(self):
         pass
@@ -61,11 +60,11 @@ class Circle(Plane):
             self.radius2 = radius2
         else:
             self.radius2 = radius
-    
+
     @property
     def area(self):
         return np.pi * np.power(self.radius, 2)
-    
+
     @property
     def circumference(self):
         return 2 * np.pi * self.radius
@@ -77,22 +76,31 @@ class Circle(Plane):
 
     def __eq__(self, other):
         return (all(self.center == other.center) and
-            all(self.normal_vector == other.normal_vector) and
-            self.radius == other.radius)
+                all(self.normal_vector == other.normal_vector) and
+                self.radius == other.radius)
 
     def has_point(self, point):
-        # a point is in the circle if the point is on th plane and 
+        # a point is in the circle if the point is on th plane and
         # the distance to origin is less than radius
         # TODO: support for ellipse
-        return (super().has_point(point) and 
-            np.linalg.norm(np.array(point)-self.center) < self.radius)
+        return (super().has_point(point) and
+                np.linalg.norm(np.array(point)-self.center) < self.radius)
+
+
+class Triangle(Plane):
+    pass
+
+
+class Polygon(Plane):
+    pass
 
 
 class Rectangle(Plane):
     def __init__(self, p, va, vb):
         super().__init__(p, np.cross(va, vb))
-        self.va = np.array(va) 
+        self.va = np.array(va)
         self.vb = np.array(vb)
+
     def has_point(self, point):
         diag = np.abs(self.va + self.vb)
         if super().has_point(point) and all(diag >= np.abs(point-self.p)):
@@ -102,19 +110,29 @@ class Rectangle(Plane):
 
 
 class Sphere(object):
-    def __init__(self, center, radius):
+    """ Sphere with direction and angle """
+    def __init__(self, center, radius, axis_vector, angle=np.pi):
         self.center = np.array(center)
         self.radius = np.float(radius)
+        if angle > 0 and angle <= np.pi:
+            self.angle = angle
+        else:
+            raise ValueError()
+        self.axis_vector = np.array(axis_vector)
 
     def has_point(self, point):
-        if type(point) is not np.ndarray:
-            point = np.array(point)
-        dist = np.linalg.norm(self.center - point)
-        return (dist - self.radius) < np.finfo(float).eps
+        v = point - self.center
+        dist = np.linalg.norm(v)
+        if (dist - self.radius) < np.finfo(float).eps:
+            costheta = np.dot(self.axis_vector, v) / (dist * self.radius)
+            if costheta < self.angle:
+                return True
+        return False
 
 
 class Barrel(object):
     """ Cylinder surface """
+
     def __init__(self, axis_vector, radius, center):
         self.axis_vector = np.array(axis_vector)
         self.radius = radius
@@ -124,13 +142,10 @@ class Barrel(object):
         self.unit_vector = Vec3.normalize(self.axis_vector)
         self.axis = Segment(center, axis_vector)
 
-
     def has_point(self, point):
         if type(point) is not np.ndarray:
             point = np.array(point)
         foot = self.axis.find_foot(point)
         dist = self.axis.distance_to_point(point)
         return (dist - self.radius < np.finfo(float).eps and
-            self.axis.has_point(foot))
-    
-
+                self.axis.has_point(foot))
--- pyHIFU/geometric/volume.py
+++ pyHIFU/geometric/volume.py
@@ -1,21 +1,42 @@
 import numpy as np
-# a volume is a combination of different surfaces. 
+from pyHIFU.geometric.vec3 import Vec3
+from pyHIFU.geometric.lines import Line, Ray, Segment
+from pyHIFU.geometric.surfaces import Plane, Circle, Sphere, Rectangle
+# a volume is a combination of different surfaces.
 
-class Ball(object):
+
+class Volume(list):
+    def __init__(self, face_list, adj_mtx=None, config_file=None):
+        pass
+
+
+class Ball(Volume):
     def __init__(self, radius=None, normal_vector=None, angle=np.pi):
         pass
 
 
-class Cylinder(object):
+class Cylinder(Volume):
     def __init__(self, radius=None, axis_vector=None, length=None):
         pass
 
 
+class Cuboid(Volume):
+    def __init__(self, o, a, b, c):
+        self.o1 = np.array(o)
+        self.a = np.array(a)
+        self.b = np.array(b)
+        self.c = np.array(c)
+        self.o2 = self.o1 + self.a + self.b + self.c
 
-class Volume(list):
-    def __init__(self, adj_mtx=None, config_file=None):
-        pass
 
-    def 
+        self.face_ab_1 = Rectangle(self.o1, self.a, self.b)
+        self.face_bc_1 = Rectangle(self.o1, self.b, self.c)
+        self.face_ac_1 = Rectangle(self.o1, self.a, self.c)
+        self.face_ab_2 = Rectangle(self.o2, -self.a, -self.b)
+        self.face_bc_2 = Rectangle(self.o2, -self.b, -self.c)
+        self.face_ac_2 = Rectangle(self.o2, -self.a, -self.c)
 
-        
+        face_list = [self.face_ab_1, self.face_bc_1, self.face_ac_1,
+                     self.face_ab_2, self.face_bc_2, self.face_ac_2]
+
+        adj_mtx = []
