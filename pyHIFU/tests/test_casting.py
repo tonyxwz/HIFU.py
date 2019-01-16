085ce6a708a1b220a7efc6cf0d93858074d3e296
tonyxwz <tonyxwz@yahoo.com>
2019-01-16 17:31:00 +0100

    tranducer element implemented
    together with corresponding tests

--- pyHIFU/tests/test_casting.py
+++ pyHIFU/tests/test_casting.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numba import vectorize
 import unittest
+from pyHIFU.transducer import TElement, Transducer
+from pyHIFU.geometric.vec3 import EPS
 
 
 @vectorize(['float32(float32, float32)'], target='cuda')
@@ -17,3 +19,28 @@ class Test_NumbaCuda(unittest.TestCase):
         # Add arrays on GPU
         C = Add(A, B)
         self.assertTrue(np.all(C == A*2))
+
+class Test_TransducerElement(unittest.TestCase):
+    
+
+    def test_buildtridents(self):
+        tcenter = [-14, 0, 0]
+        elid = 0
+        te = TElement(tcenter, elid)
+        for tr in te:
+            a =np.dot(tr.pow_ray.d, te.axial_ray.d)
+            costheta = a / (np.linalg.norm(tr.pow_ray.d) * np.linalg.norm(te.axial_ray.d))
+            self.assertTrue(costheta >= np.sqrt(2)/2)
+
+    def test_tridentangle(self):
+        tcenter = [-14, 0, 0]
+        elid = 0
+        te = TElement(tcenter, elid)
+        for tr in te:
+            costheta = np.dot(tr.pow_ray.d, tr.aux_ray1.d) / (np.linalg.norm(tr.pow_ray.d) *
+                                                             np.linalg.norm(tr.aux_ray1.d))
+            self.assertTrue(costheta - np.cos(1e-4) < EPS)
+            n1 = np.cross(tr.pow_ray.d, tr.aux_ray1.d)
+            n2 = np.cross(tr.pow_ray.d, tr.aux_ray2.d)
+            self.assertTrue(np.abs(np.dot(n1, n2)) < EPS)
+
--- pyHIFU/transducer.py
+++ pyHIFU/transducer.py
@@ -1,19 +1,54 @@
 import numpy as np
 from .geometric.surfaces import Sphere
+from .geometric.lines import Ray
+from .geometric.surfaces import Plane
+from .geometric.curves import Ring
 from .ray import Trident
 from .io.config import readjson
+from .geometric.vec3 import Vec3
 
 
 class TElement(list):
     """ tranducer element class """
-    def __init__(self, coor, n_rays, el_idx, **kw):
+    def __init__(self, center, el_idx, trident_angle=1e-4,
+                 n_rays=500, sampling_angle=np.pi/4, **kw):
         super().__init__()
-        self.coor = coor
+        self.center = center # also the start of all tridents
+        self.el_idx = el_idx
+        self.sampling_sphere = Sphere(center, 1, np.array([0,0,0])-self.center,
+                                       angle=np.pi/2)
+        self.axial_ray = Ray(center, np.array([0,0,0])-self.center)
+        v_radial_origin = self.axial_ray.perpendicularDirection()
         for i in range(n_rays):
-            self.append(Trident(coor, [0,1,0],
-                                coor, [0,1,1e-4],
-                                coor, [1e-4,1,0],
-                                el_idx=el_idx, ray_idx=i))
+            alpha = np.random.random() * sampling_angle
+            p1 = self.axial_ray.to_coordinate(np.cos(alpha))
+            # plane1 = Plane(p1, axial_ray.d)
+            # ring = Ring(p1, np.sin(alpha), axial_ray.d)
+            # random angle on the ring by counter-clockwise rotation
+            beta = np.random.random() * np.pi * 2
+            v_ = Vec3.rotate(v_radial_origin, self.axial_ray.d, beta)
+
+            p_end = p1 + Vec3.normalize(v_)*np.sin(alpha)
+            pow_dire = p_end - self.center
+            ray_helper = Ray(self.center, pow_dire)
+            # two perpendicular directions for auxrays
+            n_helper = ray_helper.perpendicularDirection()
+            n2_helper = np.cross(pow_dire, n_helper)
+            l = np.tan(trident_angle) * np.linalg.norm(pow_dire)
+            assert l > 0
+            a1_end = p_end + Vec3.normalize(n_helper) * l
+            a2_end = p_end + Vec3.normalize(n2_helper) * l
+            
+            self.append(Trident(self.center, pow_dire,
+                                self.center, a1_end-self.center,
+                                self.center, a2_end-self.center,
+                                el_idx=self.el_idx, ray_idx=self.el_idx*n_rays+i,
+                                med_idx=0)) # med_idx = 0: lossless
+            
+    @staticmethod
+    def initial_trident(start, angle):
+        pass
+
 
     def cast(self, n):
         pass
@@ -26,7 +61,7 @@ class Transducer(list):
         el_list = config['element_list']
         for i,el in enumerate(el_list):
             # TODO: set ray number
-            self.append(TElement(el["coordinates"], 1, i))
+            self.append(TElement(el["coordinates"], i))
 
     def initialize(self):
         pass
