897431fbe7e2289193cd2b2ba5760aa25a5231e7
tonyxwz <tonyxwz@yahoo.com>
2019-01-16 16:52:30 +0100

    fixed mutual import problem

--- pyHIFU/ray.py
+++ pyHIFU/ray.py
@@ -4,7 +4,8 @@ import numpy as np
 
 from .geometric.lines import Ray as GeoRay
 from .geometric.surfaces import Plane
-
+from pyHIFU.physics import snell, fresnel
+from pyHIFU.geometric.voxel import Voxel
 
 class Ray(GeoRay):
     def __init__(self, start=None, direction=None):
@@ -12,6 +13,12 @@ class Ray(GeoRay):
 
     @property
     def end(self):
+        pass
+
+    def v_reflect(self, interface):
+        pass
+
+    def v_refract(self, interface):
         pass
 
 
@@ -25,6 +32,9 @@ class PowRay(Ray):
         self.initial_phase = initial_phase
         # self.end = 0 # calculate the end of the vector acc. medium
 
+    def powsnell(self):
+        pass
+
 
 class AuxRay(Ray):
     def __init__(self, start=None, direction=None,
@@ -36,6 +46,8 @@ class AuxRay(Ray):
         self.initial_phase = initial_phase
         # self.end = 0 # calculate the end of the vector acc. medium
 
+    def auxsnell(self):
+        pass
 
 class Trident(object):
     """
@@ -50,6 +62,9 @@ class Trident(object):
         self.pow_ray = PowRay(start=start_pow, direction=dire_pow)
         self.aux_ray1 = AuxRay(start=start_aux1, direction=dire_aux1)
         self.aux_ray2 = AuxRay(start=start_aux2, direction=dire_aux2)
+        self.el_idx=el_idx # element index
+        self.id = ray_idx # ray id
+        self.med_idx = med_idx # medium index
 
     def __str__(self):
         return str(self.__dict__)
@@ -65,4 +80,11 @@ class Trident(object):
         return np.linalg.norm(np.cross(p1-p0, p2-p0)) / 2
 
     def get_power_at(self, distance):
+        pass
+
+    def trisnell(self):
+        # get "snelled" tridents, call `powsnell` and `auxsnell`
+        pass
+
+    def intersect_voxel(self, v:Voxel):
         pass
