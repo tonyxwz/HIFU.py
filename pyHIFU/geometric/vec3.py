9e2c58093b98189c50eb1b1f8d86fd508b447b75
tonyxwz <tonyxwz@yahoo.com>
2019-01-16 16:50:02 +0100

    rotation matrix according to Euler-Rodrigues formula

--- pyHIFU/geometric/vec3.py
+++ pyHIFU/geometric/vec3.py
@@ -26,3 +26,36 @@ class Vec3(object):
     @staticmethod
     def distance(p1, p2):
         np.sqrt(p2 - p1)
+    @staticmethod
+    def is_infinitesimal(n):
+        return n < EPS
+
+    @staticmethod
+    def is_zero(n):
+        return Vec3.is_infinitesimal(n)
+
+    @staticmethod
+    def rotation_matrix(axis, theta):
+        """
+        Return the rotation matrix associated with counterclockwise rotation about
+        the given axis by theta radians.
+        """
+        axis = np.asarray(axis)
+        axis = axis / np.sqrt(np.dot(axis, axis))
+        a = np.cos(theta / 2.0)
+        b, c, d = -axis * np.sin(theta / 2.0)
+        aa, bb, cc, dd = a * a, b * b, c * c, d * d
+        bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+        return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                        [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                        [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+    @staticmethod
+    def rotate(v, axis, theta):
+        return np.dot(Vec3.rotation_matrix(axis, theta), v)
+
+    @staticmethod
+    def alt_rotation_matrix(axis, theta):
+        from scipy.linalg import expm, norm
+        from numpy import cross, eye, dot
+        return expm(cross(eye(3), axis/norm(axis)*theta))
