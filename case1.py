6f709712c55c53b7aa74d5dc3816a0d6150aaebb
tonyxwz <tonyxwz@yahoo.com>
2019-01-29 20:04:35 +0100

    plots
    visualizeing transducer element coordinate, tridents in element and
    markoil boundary

--- case1.py
+++ case1.py
@@ -1,54 +1,35 @@
-# case one: no interfaces, ray casted from one transducer to cube medium
-# compare result from traditional method
+# case one: no interfaces, ray casted from one transducer to infinite markoil medium
+# sample on cube and compare result from traditional method
 
 from pyHIFU.transducer import Transducer
 from pyHIFU.io.config import readjson
-
-config = readjson(json_path='data/example2.json')
-transducer_config = config['transducer']
-T = Transducer(transducer_config)
-
+import numpy as np
 from pyHIFU.physics.medium import MediaComplex
 
-mc = MediaComplex(config_json=config)
-
-from math import inf
-import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-t = inf
-interface = None
+from mpl_toolkits.mplot3d import Axes3D
+from pyHIFU.visualization.mkplots import *
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
 
-n = 100
-pps = []
-pa1s = []
-pa2s = []
+config = readjson(json_path='data/test_case1.json')
+transducer_config = config['transducer']
+mc = MediaComplex(config_json=config)
+coor_file_path = "data/transducer_position.txt"
+textarray = np.loadtxt(coor_file_path)
+coordinates = textarray[::3,1:]
 
-for i,el in enumerate(T):
-    print("element", i)
-    for j,tr in enumerate(el):
-        print("trident", j)
-        for k, m in enumerate(mc):
-            print("medium", k)
-            for x,f in enumerate(m.shape):
-                print("face:", x)
-                pp = tr.pow_ray.intersect_plane(f)
-                pa1 = tr.aux_ray1.intersect_plane(f)
-                pa2 = tr.aux_ray2.intersect_plane(f)
-                if (pp is not None):
-                    l = np.linalg.norm(pp-tr.pow_ray.start)
-                    if t > l:
-                        t = l
-                        interface = f
-                    pps.append(pp)
-                    print("pp:", pp)
-                if pa1 is not None:
-                    pa1s.append(pa1)
-                    print("pa1", pa1)
-                if pa2 is not None: 
-                    pa2s.append(pa2)
-                    print("pa2", pa2)
+T = Transducer(mc[0], element_coordinates=coordinates, **transducer_config)
+T.initialize()
+T.initial_cast()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plot_transducer(T, ax)
+plot_boundary(T.init_medium.boundary, ax)
+
+plt.show()
+
+
+
+
                     
---
+++ pyHIFU/visualization/mkplots.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d as mp3d
+
+
+def plot_transducer(T, ax):
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    k = T.element_coordinates
+
+    X = k[:,0]
+    Y = k[:,1]
+    Z = k[:,2]
+    ax.scatter(X, Y, Z, color="r")
+    ax.scatter(*T.nature_focus, color="teal")
+    X = np.append(X, T.nature_focus[0])
+    Y = np.append(Y, T.nature_focus[1])
+    Z = np.append(Z, T.nature_focus[2])
+
+    max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max() / 2.0
+
+    mid_x = (X.max()+X.min()) * 0.5
+    mid_y = (Y.max()+Y.min()) * 0.5
+    mid_z = (Z.max()+Z.min()) * 0.5
+    ax.set_xlim(mid_x - max_range, mid_x + max_range)
+    ax.set_ylim(mid_y - max_range, mid_y + max_range)
+    ax.set_zlim(mid_z - max_range, mid_z + max_range)
+    
+    for te in T:
+        plot_TElements(te, ax)
+
+    # plt.show()
+
+def plot_TElements(te, ax):
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+
+    for tr in te:
+        xyz = np.concatenate((tr.pow_ray.p, tr.pow_ray.end))
+        xyz = xyz.reshape((2,3))
+        ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], color="m")
+
+        xyz = np.concatenate((tr.aux_ray1.p, tr.aux_ray1.end))
+        xyz = xyz.reshape((2,3))
+        ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], '--', color="g")
+        
+        xyz = np.concatenate((tr.aux_ray2.p, tr.aux_ray2.end))
+        xyz = xyz.reshape((2,3))
+        ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], '--', color="g")
+
+def plot_boundary(pl, ax):
+    x = pl.p[0]
+    vertices = [(x, -0.1, -0.1),
+        (x, -0.1, 0.1),
+        (x, 0.1, 0.1),
+        (x, 0.1, -0.1)
+        ]
+    alpha = 0.3
+    face = mp3d.art3d.Poly3DCollection([vertices], alpha=alpha, linewidth=1)
+
+    face.set_facecolor((0, 0, 1, alpha))
+    ax.add_collection3d(face)
