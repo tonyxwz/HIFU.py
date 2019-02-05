fd33a07320675e8e8cea648cf1e0e2175b53b82d
tonyxwz <tonyxwz@yahoo.com>
2019-02-05 21:13:26 +0100

    misc settings

--- pyHIFU/__init__.py
+++ pyHIFU/__init__.py
@@ -1,1 +1,8 @@
-VERSION = 0.1
+__author__ = "Weizhou Xing"
+__copyright__ = "Copyright 2019, cBio lab, Technische Universiteit Eindhoven"
+__credits__ = ["Weizhou Xing"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Rob Knight"
+__email__ = "w.xing@student.tue.nl"
+__status__ = "validating"
