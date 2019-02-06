cb5c0450a62eed4ce5f3bd3786217509deb55191
tonyxwz <tonyxwz@yahoo.com>
2019-02-06 10:56:24 +0100

    plot lattice, ray and trident

--- pyHIFU/visualization/mkplots.py
+++ pyHIFU/visualization/mkplots.py
@@ -5,6 +5,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import mpl_toolkits.mplot3d as mp3d
 from random import random
 
+from pyHIFU.geometric.volumes import Cuboid
+
+
 def plot_transducer(T, ax):
     # fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
@@ -62,16 +65,34 @@ def plot_boundary(pl, ax):
     face.set_facecolor((0, 0, 1, alpha))
     ax.add_collection3d(face)
 
-def plot_box(b, ax, title=""):
-    alpha = 0.3
+def plot_lattice(X, Y, Z, l, origin, ax, color=[]):
+    if len(color):
+        c = color
+    else:
+        alpha = 0.3
+        color = [0.3, 0.01, 0.01, alpha]
+    o = [X*l[0], Y*l[1], Z*l[2]] + origin
+    a = [l[0], 0, 0]
+    b = [0, l[1], 0]
+    c = [0, 0, l[2]]
+    
+    lattice = Cuboid(o, a, b, c)
+    plot_box(lattice, ax, color=color)
+
+def plot_box(b, ax, title="", color=[]):
+    alpha = 0.1
     
     for face in b:
         # face: rectangle
         vertices = list()
         for edge in face.edges:
             vertices.append(edge.start)
-        face = mp3d.art3d.Poly3DCollection([vertices], alpha=alpha, linewidth=1)
-        face.set_facecolor((random(), random(), random(), alpha))
+        if len(color):
+            c = color
+        else:
+            c = (random(), random(), random(), alpha)
+        face = mp3d.art3d.Poly3DCollection([vertices], alpha=alpha, linewidth=5)
+        face.set_facecolor(c)
         ax.add_collection3d(face)
     
     if len(title):
@@ -89,19 +110,25 @@ def plot_box(b, ax, title=""):
 
 
 def plot_trident(tr, ax):
-    xyz = np.concatenate((tr.pow_ray.p, tr.pow_ray.end))
-    xyz = xyz.reshape((2,3))
-    ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], color="m")
+    plot_ray(tr.pow_ray, ax, color="m", linestyle="")
+    plot_ray(tr.aux_ray1, ax, color="g", linestyle="--")
+    plot_ray(tr.aux_ray2, ax, color="g", linestyle="--")
 
-    xyz = np.concatenate((tr.aux_ray1.p, tr.aux_ray1.end))
+def plot_ray(r, ax, color="m", linestyle=""):
+    xyz = np.concatenate((r.p, r.end))
     xyz = xyz.reshape((2,3))
-    ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], '--', color="g")
-    
-    xyz = np.concatenate((tr.aux_ray2.p, tr.aux_ray2.end))
-    xyz = xyz.reshape((2,3))
-    ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], '--', color="g")
+    ax.plot(xyz[:,0], xyz[:,1], xyz[:,2], linestyle, color=color)
 
 def plot_reflection(tr, interface, ax):
     # TODO: the reflection progress of one transducer on one interface
     # also give resulted shear ray and longitudinal ray different color
     pass
+
+if __name__ == "__main__":
+    from pyHIFU.ray import AuxRay
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    r = AuxRay([1.3, 0.57, 0.34], [-0.13, -0.057, -0.034])
+    r.end = [0, 0, 0]
+    plot_ray(r, ax)
+    plt.show()
